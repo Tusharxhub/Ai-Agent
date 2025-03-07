ce8d8a4c3fe071a13537089453b08c55684082b5
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-07 23:24:01 +0530

    Add CORS headers to Vite server configuration for enhanced security

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    headers:{
+      "Cross-origin-embedder-policy": "require-corp",
+      "Cross-origin-opener-policy": "same-origin",
+    }
+  }
 })
