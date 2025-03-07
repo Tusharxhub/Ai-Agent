25da9e8c56b2773dfe21beb5f0905e5ab14fc985
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-07 19:56:32 +0530

    Refactor server and app imports; add morgan for logging; update package dependencies

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,8 @@ import projectRoutes from './routes/project.routes.js';
 import aiRoutes from './routes/ai.routes.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-connect();
 
+connect();
 
 const app = express();
 
@@ -19,12 +19,10 @@ app.use(cookieParser());
 
 app.use('/users', userRoutes);
 app.use('/projects', projectRoutes);
-app.use("/ai", aiRoutes)
-
-
+app.use('/ai', aiRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-export default app; 
+export default app;
