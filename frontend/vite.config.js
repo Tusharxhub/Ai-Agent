1878f592bf670a04e5ceff023b087dcd299a7f4e
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-08 09:53:51 +0530

    Update Highlight.js CDN links and enhance Vite server configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    headers:{
-      "Cross-origin-embedder-policy": "require-corp",
-      "Cross-origin-opener-policy": "same-origin",
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
     },
-    proxy:{
-      "/api":{
-        target:"http://unpkg.com",
-        changeOrigin:true,
+    proxy: {
+      "/cdn": {
+        target: "http://unpkg.com",
+        changeOrigin: true,
         rewrite: (path) => path.replace(/^\/cdn/, '')
       }
     }
